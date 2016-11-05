26ed3d69205902bb648fa5add28c579b7d7770c1
Cedric Michaux <cedric@he8us.be>
2016-11-05 16:55:34 +0100

    Kali initial config

---
+++ nvm/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export NVM_DIR="$HOME/.nvm"
+
+git clone https://github.com/creationix/nvm.git $NVM_DIR
+cd $NVM_DIR
+git checkout `git describe --abbrev=0 --tags`
+
+source $NVM_DIR/nvm.sh
+
+nvm install 4.5
+nvm alias default 4.5
+nvm use 4.5
+
+npm install npm@latest -g
+npm install yarn -g
