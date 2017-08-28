99dbe2305853e5bcee8dcb59093605a4e0c1d700
Cedric Michaux <cedric@he8us.be>
2017-08-29 00:08:53 +0200

    GRC COnfig

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,41 +1,40 @@
-# GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] ))
-then
-    GRC="$(which grc)"
-    if [ "$TERM" != dumb ] && [ -n "$GRC" ]; then
-        alias colourify="$GRC -es --colour=auto"
-        alias blkid='colourify blkid'
-        alias configure='colourify ./configure'
-        alias df='colourify df'
-        alias diff='colourify diff'
-        alias docker='colourify docker'
-        alias docker-machine='colourify docker-machine'
-        alias du='colourify du'
-        alias env='colourify env'
-        alias free='colourify free'
-        alias make='colourify make'
-        alias gcc='colourify gcc'
-        alias g++='colourify g++'
-        alias ip='colourify ip'
-        alias iptables='colourify iptables'
-        alias as='colourify as'
-        alias gas='colourify gas'
-        alias ld='colourify ld'
-        #alias ls='colourify ls'
-        alias lsblk='colourify lsblk'
-        alias lspci='colourify lspci'
-        alias netstat='colourify netstat'
-        alias ping='colourify ping'
-        alias traceroute='colourify traceroute'
-        alias traceroute6='colourify traceroute6'
-        alias head='colourify head'
-        alias tail='colourify tail'
-        alias dig='colourify dig'
-        alias mount='colourify mount'
-        alias ps='colourify ps'
-        alias mtr='colourify mtr'
-        alias semanage='colourify semanage'
-        alias getsebool='colourify setsebool'
-        alias ifconfig='colourify ifconfig'
+if [[ "$TERM" != dumb ]] && (( $+commands[grc] )) ; then
+  # Prevent grc aliases from overriding zsh completions.
+  setopt COMPLETE_ALIASES
+
+  # Supported commands
+  cmds=(
+    cc \
+    configure \
+    cvs \
+    df \
+    diff \
+    dig \
+    gcc \
+    gmake \
+    ifconfig \
+    last \
+    ldap \
+    ls \
+    make \
+    mount \
+    mtr \
+    netstat \
+    ping \
+    ping6 \
+    ps \
+    traceroute \
+    traceroute6 \
+    wdiff \
+  );
+
+  # Set alias for available commands.
+  for cmd in $cmds ; do
+    if (( $+commands[$cmd] )) ; then
+      alias $cmd="grc --colour=auto $cmd"
     fi
+  done
+
+  # Clean up variables
+  unset cmds cmd
 fi
